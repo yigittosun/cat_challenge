4237b005ef46d34a6d5b85540d1c922d1210dec6
Yiğit Tosun <yigit.tosun@empa.com>
2023-08-17 23:34:22 +0300

    Install pinia for store management package to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
+
+import { defineNuxtConfig } from "nuxt/config";
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: ["bootstrap/dist/css/bootstrap.css"],
+  plugins: [
+    { src: "~/assets/plugins/bootstrap.js", mode: "client" },
+    { src: "~/assets/plugins/sweetalert.js", mode: "client" },
+  ],
+  modules: ["@pinia/nuxt"],
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.CAT_API_KEY,
+    },
+  },
+  nitro: {
+    devProxy: {
+      "/api": {
+        target: "https://api.thecatapi.com/v1",
+        changeOrigin: true,
+        prependPath: true,
+      },
+    },
+  },
+});
