eb583867e241b45e384df4c1deb66cbeac982f1c
Yiğit Tosun <yigit.tosun@empa.com>
2023-08-17 23:38:16 +0300

    Create header & footer file component

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <!--Header bar-->
+  <footer class="footer fixed-bottom bg-dark text-white text-center py-2">
+    Yiğit Tosun &copy; 2023
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({});
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- Header bar -->
+  <nav
+    class="bg-secondary bg-gradient d-flex justify-content-md-start justify-content-center"
+  >
+    <h1 class="text-dark m-3 fw-bold">Cat Challenger</h1>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({});
+</script>
