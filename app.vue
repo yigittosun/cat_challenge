b2d1fd2cdd7e2deb445b3335a30b9fc121ad83b2
Yiğit Tosun <yigit.tosun@empa.com>
2023-08-17 23:36:30 +0300

    Add header, footer & router-view component in app.vue file

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Header />
+    <router-view />
+    <Footer />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default defineComponent({
+  name: "app",
+  components: {
+    Header,
+    Footer,
+  },
+
+  setup() {
+    return {};
+  },
+});
+
+</script>
+
+<style lang="scss">
+@import "assets/sass/style.scss";
+</style>
